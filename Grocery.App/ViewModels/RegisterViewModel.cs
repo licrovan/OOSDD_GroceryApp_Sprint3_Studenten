779f643532f4cf5bfae57422cc80d4eda0fdd480
Licrovan <licrovanus@gmail.com>
2025-09-26 12:53:59 +0200

    added link from login page to register page
    added a link to the register page from login. registerpage has been created and properly added to the project refrences for maui.

--- Grocery.App/ViewModels/RegisterViewModel.cs
+++ Grocery.App/ViewModels/RegisterViewModel.cs
@@ -1,13 +1,12 @@
 ﻿
 using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
-using Grocery.App.Views;
 using Grocery.Core.Interfaces.Services;
 using Grocery.Core.Models;
 
 namespace Grocery.App.ViewModels
 {
-    public partial class LoginViewModel : BaseViewModel
+    public partial class RegisterViewModel : BaseViewModel
     {
         private readonly IAuthService _authService;
         private readonly GlobalViewModel _global;
@@ -19,40 +18,27 @@ namespace Grocery.App.ViewModels
         private string password = "user3";
 
         [ObservableProperty]
-        private string loginMessage = string.Empty;
+        private string registerMessage;
 
-        public LoginViewModel(IAuthService authService, GlobalViewModel global)
+        public RegisterViewModel(IAuthService authService, GlobalViewModel global)
         { //_authService = App.Services.GetServices<IAuthService>().FirstOrDefault();
             _authService = authService;
             _global = global;
         }
 
         [RelayCommand]
-        private void Login()
+        private void Register()
         {
             Client? authenticatedClient = _authService.Login(Email, Password);
             if (authenticatedClient != null)
             {
-                LoginMessage = $"Welkom {authenticatedClient.Name}!";
+                registerMessage = $"Welkom {authenticatedClient.Name}!";
                 _global.Client = authenticatedClient;
-                if (Application.Current != null)
-                {
-                    Application.Current.MainPage = new AppShell();
-                }
+                Application.Current.MainPage = new AppShell();
             }
             else
             {
-                LoginMessage = "Ongeldige inloggegevens.";
-            }
-        }
-
-        [RelayCommand]
-        private async Task Register()
-        {
-            if (Application.Current != null)
-            {
-                Application.Current.MainPage = new AppShell();
-                await Shell.Current.GoToAsync("Register");
+                registerMessage = "Ongeldige inloggegevens.";
             }
         }
     }
